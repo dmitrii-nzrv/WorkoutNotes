c30381e371b2ef03ceb8671b7e6545a232d7ee02
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-07-23 17:03:49 +0300

    first commit

---
+++ WorkoutNotes/WorkoutNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutNotesApp.swift
+//  WorkoutNotes
+//
+//  Created by Dmitrii Nazarov on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
